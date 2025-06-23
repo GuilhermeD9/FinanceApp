040861b160b95017b7531dea04ac56dacbb42115
GuilhermeD9 <sgsouza.173@gmail.com>
2025-06-23 09:10:06 -0300

    feat: Primeira versão funcional do projeto

--- app/src/main/java/dev/guilherme/financeapp/MainActivity.kt
+++ app/src/main/java/dev/guilherme/financeapp/MainActivity.kt
@@ -1,20 +1,38 @@
 package dev.guilherme.financeapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val descriptionEditText: EditText = findViewById(R.id.edit_text_description)
+        val valueEditText: EditText = findViewById(R.id.edit_text_value)
+        val addButton: Button = findViewById(R.id.button_add)
+
+        addButton.setOnClickListener{
+            val description = descriptionEditText.text.toString()
+            val valueString = valueEditText.text.toString()
+
+            if (description.isNotEmpty() && valueString.isNotEmpty()) {
+                val value = valueString.toDouble()
+
+                // Mensagem temporária
+                val message = "Transação: $description - Valor: R$$value"
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+
+                descriptionEditText.text.clear()
+                valueEditText.text.clear()
+
+            } else {
+                Toast.makeText(this, "Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
