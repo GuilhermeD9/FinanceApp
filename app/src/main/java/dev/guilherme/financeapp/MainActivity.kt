05a9e7182ef5618461e094403af29ecae32b07cb
GuilhermeD9 <sgsouza.173@gmail.com>
2025-06-23 12:00:50 -0300

    fix: Correcao na lista de items

--- app/src/main/java/dev/guilherme/financeapp/MainActivity.kt
+++ app/src/main/java/dev/guilherme/financeapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package dev.guilherme.financeapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val transactions = mutableListOf<Transaction>()
+    private lateinit var adapter: TransactionAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,6 +21,11 @@ class MainActivity : AppCompatActivity() {
         val descriptionEditText: EditText = findViewById(R.id.edit_text_description)
         val valueEditText: EditText = findViewById(R.id.edit_text_value)
         val addButton: Button = findViewById(R.id.button_add)
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view_transactions)
+
+        adapter = TransactionAdapter(transactions)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         addButton.setOnClickListener{
             val description = descriptionEditText.text.toString()
@@ -21,18 +33,17 @@ class MainActivity : AppCompatActivity() {
 
             if (description.isNotEmpty() && valueString.isNotEmpty()) {
                 val value = valueString.toDouble()
+                val transaction = Transaction(description, value)
 
-                // Mensagem temporária
-                val message = "Transação: $description - Valor: R$$value"
-                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                transactions.add(transaction)
+
+                adapter.notifyItemInserted(transactions.size - 1)
 
                 descriptionEditText.text.clear()
                 valueEditText.text.clear()
-
             } else {
                 Toast.makeText(this, "Por favor, preencha todos os campos", Toast.LENGTH_SHORT).show()
             }
-
         }
     }
 }
--- app/src/main/java/dev/guilherme/financeapp/TransactionAdapter.kt
+++ app/src/main/java/dev/guilherme/financeapp/TransactionAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class TransactionAdapter(private val transactions: List<Transaction>) :
+class TransactionAdapter(private val transactions: MutableList<Transaction>) :
     RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
 
     class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
