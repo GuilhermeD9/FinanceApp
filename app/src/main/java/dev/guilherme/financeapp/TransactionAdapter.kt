ae239cc94f70e144ea8fc633a1d095e135c3b716
GuilhermeD9 <sgsouza.173@gmail.com>
2025-06-23 11:00:42 -0300

    feat: Criacao do transactionAdapter

---
+++ app/src/main/java/dev/guilherme/financeapp/TransactionAdapter.kt
@@ -0,0 +1,32 @@
+package dev.guilherme.financeapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TransactionAdapter(private val transactions: List<Transaction>) :
+    RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
+
+    class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val descriptionTextView: TextView = itemView.findViewById(R.id.text_view_item_description)
+        val valueTextView: TextView = itemView.findViewById(R.id.text_view_item_value)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.transaction_item, parent, false)
+        return TransactionViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
+        val transaction = transactions[position]
+        holder.descriptionTextView.text = transaction.description
+        holder.valueTextView.text = "R$ ${"%.2f".format(transaction.value)}"
+    }
+
+    override fun getItemCount(): Int {
+        return transactions.size
+    }
+}
