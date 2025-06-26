133df6b42a5fa0495f5b211db03461eed330aad2
GuilhermeD9 <sgsouza.173@gmail.com>
2025-06-26 09:41:54 -0300

    refactor: Removendo codigo nao utilizado

--- app/src/main/java/dev/guilherme/financeapp/Transaction.kt
+++ app/src/main/java/dev/guilherme/financeapp/Transaction.kt
@@ -1,12 +1,6 @@
 package dev.guilherme.financeapp
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "transactions")
 data class Transaction (
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
     val description: String,
     val value: Double
 )
